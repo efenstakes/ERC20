ab75fc11352c98505b2fa8c4cf05351f94badcbc
Efen Stakes <efenstakes101@gmail.com>
2022-02-15 13:35:19 +0300

    add an erc20 contract backed by openzeppelin

---
+++ contracts/OpzToken.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+
+
+contract OpzToken is ERC20, Ownable {
+    
+    constructor(uint256 initialSupply) ERC20("OpzToken", "OTK") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+
+}
